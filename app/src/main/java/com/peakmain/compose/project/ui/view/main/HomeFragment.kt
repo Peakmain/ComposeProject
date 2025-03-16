c0c2382d92ae0f8c35d87d7dbf57487d008c754e
成东(周成东) <chengdong.zhou@yaduo.com>
2025-03-16 14:53:08 +0800

    [Compose]Compose升级

--- app/src/main/java/com/peakmain/compose/project/ui/view/main/HomeFragment.kt
+++ app/src/main/java/com/peakmain/compose/project/ui/view/main/HomeFragment.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.accompanist.pager.ExperimentalPagerApi
-import com.peakmain.compose.library.Banner
 import com.peakmain.compose.library.TopAppBarCenter
 import com.peakmain.compose.project.ui.theme.Color_149EE7
 import com.peakmain.compose.project.ui.theme.Color_2DCDF5
@@ -36,28 +35,6 @@ fun HomeFragment(viewModel: HomeFragmentViewModel = viewModel()) {
         modifier = Modifier.background(Brush.linearGradient(listOf(Color_149EE7, Color_2DCDF5)))) {
 
         Column {
-            Banner(
-                data = viewModel.bannerData,//设置数据
-                onImagePath = {//设置图片的url地址
-                    viewModel.bannerData[it].imagePath
-                },
-                pagerModifier = Modifier
-                    .padding(horizontal = 16.dp)
-                    .padding(top = 10.dp)
-                    .clip(RoundedCornerShape(8.dp)),//HorizontalPager的modifier
-                pagerIndicatorModifier = Modifier
-                    .background(Color(0x90000000))
-                    .padding(horizontal = 10.dp)
-                    .padding(top = 10.dp, bottom = 10.dp),//指示器Row的整个样式
-                desc = {
-                    //指示器文本内容，也就是标题一、标题二
-                    Text(text = viewModel.bannerData[it].desc, color = Color.White)
-                }
-            ) {
-                //设置item的点击事件
-                Log.e("TAG", viewModel.bannerData[it].imagePath)
-            }
-
             Text(text = "我是首页", modifier = Modifier.padding(top = 10.dp))
         }
     }
